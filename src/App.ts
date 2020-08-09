9712d126d02dcb764a497c586cebbadff84c7a62
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2020-08-09 14:07:10 +0200

    Init basics

---
+++ src/App.ts
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import { Scene, PerspectiveCamera, WebGLRenderer } from "three";
+
+const scene = new Scene();
+const camera = new PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  renderer.render(scene, camera);
+}
+
+export default Vue.extend({
+  name: "App",
+  beforeMount() {
+    animate();
+  },
+});
